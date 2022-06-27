16db1ff2b53888f380dee14eae0528ced372aae9
Chris <info@chrisgkalfas.com>
2022-06-27 23:35:14 +0300

    Initial commit

---
+++ src/Components/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/MainPage.css'
+
+
+function MainPage() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default MainPage
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/NavBar.css'
+
+function NavBar() {
+  return (
+    <div>
+      <h1>Github Users</h1>
+    </div>
+  )
+}
+
+export default NavBar
